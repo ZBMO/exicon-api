0e839f97d07146e65d6088c5c63d1186b4b046c7
zach <zachariah.eb@protonmail.com>
2022-10-25 16:07:20 -0500

    other changes

---
+++ src/main/kotlin/com/exicon/exicon/ExiconApplication.kt
@@ -0,0 +1,11 @@
+package com.exicon.exicon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExiconApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExiconApplication>(*args)
+}
---
+++ src/test/kotlin/com/exicon/exicon/ExiconApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exicon.exicon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExiconApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
